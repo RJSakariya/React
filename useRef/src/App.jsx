66cfbb7d37c39ed6372ae344a68f8c44b75a3234
RJSakariya <rjsakariya5@gmail.com>
2024-09-15 18:07:41 +0530

    useRef

--- useRef/src/App.jsx
+++ useRef/src/App.jsx
@@ -4,24 +4,26 @@ import "./App.css"
 export default function App() {
   const Name = useRef("");
   const Password = useRef("");
-  useEffect(()=>{
-    setTimeout(()=>{
+  useEffect(() => {
+    setTimeout(() => {
       Name.current.focus()
-    },1000)
-  },[])
+    }, 1000)
+  }, [])
   return (
-    <div className="container">
-      <div className="row align-items-center justify-content-center" style={{height:"100vh"}}>
-        <div className="col-12 col-md-8 col-lg-6 col-xl-4 card px-4 py-5">
-          <input ref={Name} type="text" className="form-control" placeholder="Enter Your Name"/>
-          <input ref={Password} type="password" className="form-control mt-3" placeholder="Enter Your Password"/>
-          <button className="btn btn-primary mt-4" onClick={
-            () => {
-              Name.current.value === ""? Name.current.focus() : Password.current.value === "" ? Password.current.focus() : alert("Name : " + Name.current.value+", Password : " + Password.current.value)
-            }
-          }>Submit</button>
+    <>
+      <div className="container">
+        <div className="row align-items-center justify-content-center" style={{ height: "100vh" }}>
+          <div className="col-12 col-md-8 col-lg-6 col-xl-4 card px-4 py-5">
+            <input ref={Name} type="text" className="form-control" placeholder="Enter Your Name" />
+            <input ref={Password} type="password" className="form-control mt-3" placeholder="Enter Your Password" />
+            <button className="btn btn-primary mt-4" onClick={
+              () => {
+                Name.current.value === "" ? Name.current.focus() : Password.current.value === "" ? Password.current.focus() : alert("Name : " + Name.current.value + ", Password : " + Password.current.value)
+              }
+            }>Submit</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
